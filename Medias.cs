c9cd6532eeeab5aba6c8cc4b60deb12cf052fc46
SztyLeAndres <amartinez31@ucol.mx>
2019-03-14 10:28:28 -0600

    Problema 00

--- Medias.cs
+++ Medias.cs
@@ -11,7 +11,21 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(params int[] vals) { 
+
+            double sumaTotal = 0;
+            double v1 = 0;
+            double v2 = 0;
+            double promedio = 0;
+
+            for (int =1; i < vals.Length;i++) {
+                v1 = vals[i];
+                v2 = vals[i+1];
+                sumaTotal = v1+v2;
+            }
+            promedio = sumaTotal/vals.Length;
+            return proemdio;
+        }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
